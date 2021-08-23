00a83556997a10202321037819377fe3c9f057ec
SaadYum <saadrehman415@gmail.com>
2021-08-23 11:39:46 +0500

    final code

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "light-blue": colors.sky,
+        cyan: colors.cyan,
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+    scrollbar: ["rounded"],
+  },
+  plugins: [require("tailwind-scrollbar")],
+};
